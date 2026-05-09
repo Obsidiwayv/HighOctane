758cba85c9e5915d5141cd5cafa9726fbeb161ae
Obsidiwayv <wayvlyte@gmail.com>
2026-05-09 13:42:32 -0400

    Add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM gradle:9.5-jdk25-noble
+COPY --chowne-gradle:gradle . /home/gradle/src/
+WORKDIR /home/gradle/src
+RUN gradle shadowJar --no-daemon
+
+FROM eclipse-temurin:25-jre-noble
+ENTRYPOINT ["java", "-jar", "build/libs/HighOctane-1.0-all.jar"]
--- src/main/java/wayvlyte/space/octane/OctaneConfig.java
+++ src/main/java/wayvlyte/space/octane/OctaneConfig.java
@@ -2,14 +2,32 @@ package wayvlyte.space.octane;
 
 import com.electronwill.nightconfig.core.file.FileConfig;
 
+import java.io.File;
+
 public class OctaneConfig {
-    private static final FileConfig Conf = FileConfig.of("client.config.toml");
+    private static final String ConfigFileName = "client.config.toml";
+    private static final FileConfig Conf = FileConfig.of(ConfigFileName);
+    /**
+     * Property that determines if this class will use either use Environment Values or A TOML file
+     */
+    private static boolean bHasConfig = true;
 
     public static void Load() {
+        File ConfigFile = new File(ConfigFileName);
+        // Check if the file exists if not then fallback to the servers environment
+        if (!ConfigFile.exists()) {
+            System.out.println("Unable to load config file, environment values will be used when using Get(key)");
+            bHasConfig = false;
+            return;
+        }
         Conf.load();
     }
 
-    public static <T>T Get(String Key) {
+    public static String Get(String Key) {
+        if (!bHasConfig || !Conf.contains(Key)) {
+            String EnvName = Key.replace(".", "_").toUpperCase();
+            return System.getenv(EnvName);
+        }
         return Conf.get(Key);
     }
 }
